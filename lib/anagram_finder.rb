79030a51c2cc5d9a785fc5748ac6472979adb17a
Chris LeCompte & Jessi Iler <student@epicodus.com>
2015-04-23 14:53:49 -0700

    Wraps all anagrams up in pithy list

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -12,6 +12,10 @@ class Array
         list_array.push(word)
       end
     end
-    list_array
+    pithy_response = list_array.join(", ")
+    pithy_sentence = []
+    pithy_sentence.push("Your amazing anagram matches are: ")
+    pithy_sentence.push(pithy_response)
+    pithy_sentence_final = pithy_sentence.join()
     end
 end
--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -5,7 +5,11 @@ describe('Array#anagram_finder') do
   #it('turns a given list into an array') do
   #  expect((["cat", "act", "tac"]).anagram_finder()).to(eq("cat"))
   #end
-  it('checks if words are anagrams') do
-    expect((["cat", "act", "movie", "tac"]).anagram_finder()).to(eq(["act", "tac"]))
+  #it('checks if words are anagrams') do
+  #  expect((["cat", "act", "movie", #"tac"]).anagram_finder()).to(eq(["act", "tac"]))
+#end
+
+it('returns pithy response to input') do
+  expect((["cat", "act", "movie", "tac"]).anagram_finder()).to(eq("Your amazing anagram matches are: act, tac"))
 end
 end
