c95fec0764c0632bf56a62073f0ba5d1a858b134
Chris LeCompte & Jessi Iler <student@epicodus.com>
2015-04-23 14:37:34 -0700

    now sorts anagrams

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,8 +1,17 @@
 class Array
-
   define_method(:anagram_finder) do
+    word_array = self
+    super_word = word_array.at(0)
+    word_array.shift()
+    super_word_array = super_word.split("")
+    list_array = []
 
-super_word = self.at(0)
-
-end
+    word_array.each() do |word|
+      split_word = word.split("").sort()
+      if super_word_array.sort().==(split_word)
+        list_array.push(word)
+      end
+    end
+    list_array
+    end
 end
--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('anagram_finder')
 
 describe('Array#anagram_finder') do
-  it('turns a given list into an array') do
-    expect((["cat", "act", "tac"]).anagram_finder()).to(eq("cat"))
-  end
+  #it('turns a given list into an array') do
+  #  expect((["cat", "act", "tac"]).anagram_finder()).to(eq("cat"))
+  #end
+  it('checks if words are anagrams') do
+    expect((["cat", "act", "movie", "tac"]).anagram_finder()).to(eq(["act", "tac"]))
+end
 end
